3a3af91c32d0d9d3f20fedc0b0b9d607998d80aa
MikeYatsenko <yatsbb@gmail.com>
2021-06-09 21:00:23 +0300

    updated permissions

---
+++ mike/mike/celery.py
@@ -0,0 +1,6 @@
+import os
+from celery import Celery
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_parroter.settings')
+celery_app = Celery('mike')
+celery_app.config_from_object('django.conf:settings', namespace='CELERY')
+celery_app.autodiscover_tasks()
