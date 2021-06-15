cedb91cdaad8652e0f293717375f637a05947410
MikeYatsenko <yatsbb@gmail.com>
2021-06-16 00:18:37 +0300

    started celery

--- mike/mike/celery.py
+++ mike/mike/celery.py
@@ -1,6 +1,6 @@
 import os
 from celery import Celery
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_parroter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mike.settings')
 celery_app = Celery('mike')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
 celery_app.autodiscover_tasks()
--- mike/mike/settings.py
+++ mike/mike/settings.py
@@ -190,3 +190,10 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Moscow'
